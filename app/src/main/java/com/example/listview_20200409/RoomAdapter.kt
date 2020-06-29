d9f678b6f423d38a1072d96d43e779ad6669c668
peunj04 <peunj04@gmail.com>
2020-06-29 23:10:55 +0900

    실 데이터 리스트에 반영

--- app/src/main/java/com/example/listview_20200409/RoomAdapter.kt
+++ app/src/main/java/com/example/listview_20200409/RoomAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import kotlin.math.floor
 
 class RoomAdapter(context : Context, resId : Int, list :ArrayList<Room>) : ArrayAdapter<Room>(context, resId, list){
 
@@ -22,6 +24,43 @@ class RoomAdapter(context : Context, resId : Int, list :ArrayList<Room>) : Array
         val row=tempRow!!
 
         //row변수를 활용해서 각줄에 맞는 데아터 가공.
+
+        //XML에 있는 뷰들을 변수로 담기. findViewById
+        val priceTxt=row.findViewById<TextView>(R.id.priceTxt)
+        val addressAndFloorTxt=row.findViewById<TextView>(R.id.addressAndFloorTxt)
+        val descTxt=row.findViewById<TextView>(R.id.descTxt)
+
+        //근거 데이터 변수 추출.=>각 위치에 맞는 데이터를 mList에서 가져오기
+        val data=mList.get(position)
+
+        //가격이 1만 이상? "?억 ?" 식, 그 이하 "?"
+        if (data.price>=10000){
+            priceTxt.text="${data.price/10000}억 ${String.format("%d",data.price%10000)}"
+        }
+
+        else{
+            priceTxt.text=String.format("%d", data.price)
+        }
+
+        //주소와 층수=>"주소, 층수(상황마다다른값)"
+
+        var floorStr=""
+
+        if(data.floor==0){
+            floorStr="반지하"
+        }
+        else if(data.floor>0){
+            floorStr="${data.floor}층"
+        }
+        else{
+            floorStr="지하 ${-data.floor}층"
+        }
+
+        addressAndFloorTxt="${data.address}, ${floorStr}"
+
+        //설명은 그냥 있는 그대로 대입
+
+        descTxt.text=data.desc
 
         return row
     }
